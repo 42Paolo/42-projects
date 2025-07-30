4dfdc5f70c539f637f4ffb93282111363683c039
42Paolo <emailcreatasulmomentoo@gmail.com>
2025-07-30 22:07:40 +0200

    add: changes to ex07

--- piscine-part/C00/ex06/ft_print_comb2.c
+++ piscine-part/C00/ex06/ft_print_comb2.c
@@ -6,11 +6,19 @@
 /*   By: pbrogi <pbrogi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 00:49:25 by pbrogi            #+#    #+#             */
-/*   Updated: 2025/07/29 01:06:17 by pbrogi           ###   ########.fr       */
+/*   Updated: 2025/07/30 22:07:23 by pbrogi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
+void	ft_print_num(int n)
+{
+	char c;
+	
+	c = n + '0';
+	write(1, &c, 1);
+}
 
 void    ft_print_comb2()
 {
@@ -19,5 +27,44 @@ void    ft_print_comb2()
 	
 	int u_2 = 0;
 	int d_2 = 0;
-    
+	
+	while(d_2 != 10)
+	{
+		while(u_2!= 10)
+		{
+			while(d_1 != 10)
+			{
+				while(u_1 != 10)
+				{
+					if(1)
+					{
+
+						ft_print_num(d_2);
+						ft_print_num(u_2);
+						write(1, " ", 1);
+						ft_print_num(d_1);
+						ft_print_num(u_1);
+						write(1, ", ", 2);
+						u_1++;
+					}
+				}
+				
+				u_1 = 0;
+				d_1++;
+				
+			}
+			d_1 = 0;
+			u_2++;
+		}
+		u_2 = 0;
+		d_2++;
+	}
+	write(1, "\n", 1);
+}
+
+int main(void)
+{
+	ft_print_comb2();
+
+	return (0);
 }
--- piscine-part/C00/ex07/ft_putnbr.c
+++ piscine-part/C00/ex07/ft_putnbr.c
@@ -6,16 +6,16 @@
 /*   By: pbrogi <pbrogi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 03:50:47 by pbrogi            #+#    #+#             */
-/*   Updated: 2025/07/29 03:50:59 by pbrogi           ###   ########.fr       */
+/*   Updated: 2025/07/30 20:52:32 by pbrogi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
 void	ft_putnbr(int num)
-{   
-	char char_num;
-	
+{
+	char	char_num;
+
 	if (num < 0)
 	{
 		write(1, "-", 1);
